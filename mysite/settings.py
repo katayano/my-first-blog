0634578f7ca95f55f158a8563c3cb13f593a934a
katayama <kata.127.yama@gmail.com>
2022-11-27 22:53:14 +0900

    change SECRET_KEY settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,8 @@ import os
 try:
     from .local_settings import *
 except ImportError:
-    pass
+    from django.core.management.utils import get_random_secret_key
+    SECRET_KEY = get_random_secret_key()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
